e9610e62fbb1561db6a5de316c8a18ef50f04287
rohitazad <ram9953878727@gmail.com>
2024-10-17 16:04:42 +0530

    config add for build version

--- next.config.js
+++ next.config.js
@@ -1,4 +1,17 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
 
-module.exports = nextConfig
+const { execSync } = require('child_process');
+
+
+module.exports = {
+    generateBuildId: async () => {
+      try {
+        // Fetch the latest commit hash
+        const commitHash = execSync('git rev-parse HEAD').toString().trim();
+        return commitHash; // Return the commit hash as the build ID
+      } catch (error) {
+        console.error('Error fetching commit hash:', error);
+        return 'build'; // Fallback ID if git command fails
+      }
+    },
+  };
--- src/app/(restpages)/contact/page.tsx
+++ src/app/(restpages)/contact/page.tsx
@@ -2,6 +2,7 @@ const Contact = ()=>{
     return (
         <>
             <h1>Contact  Page</h1>
+            <p>Current Build ID: {process.env.NEXT_BUILD_ID}</p>
         </>
     )
 }
